2c45e8f225386e408398bae2f1370bfc871e9ad9
Mislav-laptop <petrinamislav@gmail.com>
2023-12-02 19:41:52 +0100

    added prisma and connected it to postgres, created models

---
+++ backend/src/script.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            email: "admin@admin.com",
+            username: "admin",
+            password: "admin",
+            role: "ADMIN"
+        }
+    })
+
+    console.log(user)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    }) 
